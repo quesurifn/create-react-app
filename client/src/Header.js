bbd405667f85bb1debec04ceeb1498a02e0a212b
Kyle <kyle.c.r.fahey@gmail.com>
2017-08-12 13:26:20 -0500

    Update bundle being served

--- client/src/Header.js
+++ client/src/Header.js
@@ -2,16 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 
 
-import {connect} from 'react-redux'
-
-
-@connect((store) => {
-  console.log('store',store)
-  return {
-
-  }
-})
-export class Home extends Component {
+export class Header extends Component {
   render() {
   
     return (
