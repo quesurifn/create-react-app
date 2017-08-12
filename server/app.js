59a9ede025b474f52af884dc71c8ec2054e6d2d9
Kyle <kyle.c.r.fahey@gmail.com>
2017-08-12 11:45:21 -0500

    React router working with express; Heroku won't serve?

--- server/app.js
+++ server/app.js
@@ -10,7 +10,8 @@ var index = require('./routes/index');
 
 var app = express();
 
-
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
@@ -19,7 +20,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 //app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, '../client/build')));
-
 
 
 
