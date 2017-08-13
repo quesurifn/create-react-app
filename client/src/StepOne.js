d383bdf2a1764df4c0d7a5a2ad36d87d1ecb83ef
Kyle <kyle.c.r.fahey@gmail.com>
2017-08-12 20:57:14 -0500

    Step one

--- client/src/StepOne.js
+++ client/src/StepOne.js
@@ -4,6 +4,8 @@ import './App.css';
 
 import {connect} from 'react-redux'
 import {setFaceBookToken} from './actions/action'
+import AlertContainer from 'react-alert'
+
 
 import {Button} from 'react-materialize'
 import box from './images/boxoffood.png'
@@ -16,44 +18,72 @@ import box from './images/boxoffood.png'
   }
 })
 export class StepOne extends Component {
+  
   constructor() {
     super() 
+      const alertOptions = {
+    offset: 14,
+    position: 'top left',
+    theme: 'dark',
+    time: 5000,
+    transition: 'scale'
+  }
       this.isItValid = this.isItValid.bind(this);
+      this.stepTwo = this.stepTwo.bind(this)
   }
     
 
-    isItValid(el) {
-      console.log(this)
+    isItValid(e) {
+      const el = e.target 
+      if(!el.checkValidity()) {
+        el.style.borderBottom = '2px solid red';
+      } else {
+        el.style.borderBottom = '2px solid green';
+      }
 
+    }
+
+    stepTwo() {
+      if(this.refs.loginEmail.checkValidity() && this.refs.loginPassword.checkValidity() && this.refs.zipcode.checkValidity()) {
+        this.props.history.push('/steptwo')
+      } else {
+      
+         this.msg.show('Please double check your email, password and zipcode.', {
+          time: 3000,
+          type: 'error',
+           offset: 14,
+            position: 'top left',
+            theme: 'dark',
+            transition: 'scale'
+        })
+      }
     }
 
   render() {
     const responseFacebook = (response) => {
       console.log(response);
-      this.props.dispatch(setFaceBookToken(response.accessToken))
+      this.props.dispatch(setFaceBookToken(response))
     }
-
 
    
     
     return (
       <div className="container">
         <div className="row"> 
-          
-          <div className="margin80">
-          
+          <div className="margin80 stepOne">
+          <AlertContainer ref={a => this.msg = a} {...this.alertOptions} />
           
           <div className="col s6">
               <img src={box}/> 
           </div>
           <div className="col s6 stepOne">
 
-            <input ref="loginEmail" type="email" placeholder="dasda" required/>
-            <input ref="loginPassword" type="password" placeholder="Uppercase, Lowercase. 6 or more characters" required/> 
-            <input ref="zipcode" type="text" pattern="[0-9]{5}" placeholder="ex. 60010" required/>
+            <input ref="loginEmail" type="email" placeholder="email@yourdomain.com" onChange={(e) => this.isItValid(e)} required/>
+            <input ref="loginPassword" type="password" placeholder="Uppercase, Lowercase. 8 or more characters" pattern="(?=^.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$" onChange={(e) => this.isItValid(e)} required/> 
+            <input ref="zipcode" type="text" pattern="[0-9]{5}" placeholder="ex. 60010" onChange={(e) => this.isItValid(e)} required/>
             
 
-            <Button waves='light' onClick={this.isItValid(this)}>Continue</Button>
+            <Button waves='light' onClick={this.stepTwo} >Continue</Button>
             <hr />
 
             
